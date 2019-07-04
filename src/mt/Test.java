68eccb54e1773b591b051b0546859d1477cef35a
paulo <paulohess@yahoo.com>
2019-07-04 15:36:11 -0700

    initializing the prjoect

---
+++ src/mt/Test.java
@@ -0,0 +1,12 @@
+package mt;
+
+import java.io.File;
+import java.util.Date;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("\n*****************\nHello World! I made it\n*****************\n");
+	}
+
+}
